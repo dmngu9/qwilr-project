46302d65addf52a7816b4f9da6e79923ca8bec1f
dmngu9 <minh.d.nguyen@gmail.com>
2018-07-15 23:58:13 +1000

    setup

--- client/app.tsx
+++ client/app.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const App: React.StatelessComponent = () => <div>Hye ou</div>;
+// tslint:disable
+const App: React.StatelessComponent = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/about" render={() => <div>hey you</div>} />
+            <Route exact path="/profile" render={() => <div>profile</div>} />
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,20 @@
 import express, { Request, Response, Application } from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
 
+app.use(express.static('dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'hey' });
+app.get('/*', (req: Request, res: Response) => {
+    res.sendFile('index.html', { root: 'dist' });
 });
 
-app.listen(8080, () => {
-    console.log('hey started');
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server listen on port ${port}`);
 });
